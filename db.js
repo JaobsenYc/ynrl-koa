7cbefe76d875e29ac82e86c6f3b9d00aa8c6b541
JaobsenYc <chen.yang.20@ucl.ac.uk>
2023-07-24 23:58:00 +0800

    1.0.0
    添加了7个api

--- db.js
+++ db.js
@@ -1,32 +1,151 @@
-const { Sequelize, DataTypes } = require("sequelize");
+  const { Sequelize, DataTypes } = require("sequelize");
 
-// 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+  // 从环境变量中读取数据库配置
+  const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
 
-const [host, port] = MYSQL_ADDRESS.split(":");
+  const [host, port] = MYSQL_ADDRESS.split(":");
 
-const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
-  host,
-  port,
-  dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-});
+  const sequelize = new Sequelize("ynrl_test", MYSQL_USERNAME, MYSQL_PASSWORD, {
+    host,
+    port,
+    dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+  });
 
-// 定义数据模型
-const Counter = sequelize.define("Counter", {
-  count: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 1,
-  },
-});
+  // Define communities model
+  const Communities = sequelize.define("communities", {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },},{
+    timestamps: false, // 禁用 Sequelize 的自动添加 timestamp 的行为
+  });
 
-// 数据库初始化方法
-async function init() {
-  await Counter.sync({ alter: true });
-}
+  const Users = sequelize.define("user", {
+    userId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    ResidentCode: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    TotalArrears: {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    },
+    CurrentYearArrears: {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    },
+    HistoricalArrears: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    InstallationStatus: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    HeatingStatus: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    ChargeableArea: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    UserType: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    contact: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    phone_number: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    community: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    buildingId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    unitId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    floorId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    roomId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    commercial_name: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    communityId: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+  }, {
+    tableName: 'user',
+    timestamps: false,  // Assuming your table doesn't have 'createdAt' or 'updatedAt' columns.
+  });
 
-// 导出初始化方法和模型
-module.exports = {
-  init,
-  Counter,
-};
+  Communities.hasMany(Users, { as: 'Users', foreignKey: 'communityId', sourceKey: 'id' });
+  Users.belongsTo(Communities, { as: 'Community', foreignKey: 'communityId', targetKey: 'id' });
+  async function migrateCommunityId() {
+    // 获取所有 communities
+    const communities = await Communities.findAll();
+  
+    // 创建一个从 community text 映射到 id 的对象
+    const communityMap = communities.reduce((map, community) => {
+      map[community.text] = community.id;
+      return map;
+    }, {});
+  
+    // 获取所有 users
+    const users = await Users.findAll();
+  
+    // 对每个 user，根据其 community 文本设置 communityId
+    for (const user of users) {
+      user.communityId = communityMap[user.community];
+      await user.save();
+    }
+  }
+  
+  migrateCommunityId().catch(console.error);
+  
+  // 数据库初始化方法
+  async function init() {
+    try {
+      await Communities.sync({ alter: true });
+      await Users.sync({ alter: true });
+      migrateCommunityId();
+    } catch (error) {
+      console.error('Failed to synchronize the model with the database', error);
+    }
+  }
+
+  // 导出初始化方法和模型
+  module.exports = {
+    init,
+    Communities,
+    Users
+  };
--- index.js
+++ index.js
@@ -4,44 +4,180 @@ const logger = require("koa-logger");
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
-const { init: initDB, Counter } = require("./db");
+const { init: initDB, Communities , Users} = require("./db");
 
 const router = new Router();
 
 const homePage = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
 
+async function bootstrap() {
+  await sequelize.sync({ alter: true });
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
 // 首页
 router.get("/", async (ctx) => {
   ctx.body = homePage;
 });
 
-// 更新计数
-router.post("/api/count", async (ctx) => {
-  const { request } = ctx;
-  const { action } = request.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
+router.get("/api/communities", async (ctx) => {
+  if (!Communities) {
+    ctx.throw(503, "Database is not ready, please try again later");
   }
-
-  ctx.body = {
-    code: 0,
-    data: await Counter.count(),
-  };
+  const communitiesResult = await Communities.findAll();
+  ctx.body = communitiesResult.map(community => ({ id: community.id, text: community.text }))
+  ;
+});
+// // 根据community获取buildings
+router.get("/api/communities/:communityId/buildings", async (ctx) => {
+  if (!Users) {
+    ctx.throw(503, "Database is not ready, please try again later");
+  }
+  const usersResult = await Users.findAll({
+    where: { communityId: ctx.params.communityId },
+    attributes: ['buildingId'], // only select the 'buildingId' field
+    group: 'buildingId' // group by 'buildingId' to remove duplicates
+  });
+  ctx.body = usersResult.map(user => user.buildingId);
 });
 
-// 获取计数
-router.get("/api/count", async (ctx) => {
-  const result = await Counter.count();
+// // 根据buildings获取units
 
-  ctx.body = {
-    code: 0,
-    data: result,
-  };
+router.get("/api/communities/:communityId/buildings/:buildingId/units", async (ctx) => {
+  if (!Users) {
+    ctx.throw(503, "Database is not ready, please try again later");
+  }
+  const usersResult = await Users.findAll({
+    where: { 
+      communityId: ctx.params.communityId,
+      buildingId: ctx.params.buildingId 
+    },
+    attributes: ['unitId'], // only select the 'unitId' field
+    group: 'unitId' // group by 'unitId' to remove duplicates
+  });
+  ctx.body = usersResult.map(user => user.unitId );
 });
+
+// Get all floors by unit ID
+
+router.get("/api/communities/:communityId/buildings/:buildingId/units/:unitId/floors", async (ctx) => {
+  if (!Users) {
+    ctx.throw(503, "Database is not ready, please try again later");
+  }
+  const usersResult = await Users.findAll({
+    where: { 
+      communityId: ctx.params.communityId,
+      buildingId: ctx.params.buildingId,
+      unitId: ctx.params.unitId 
+    },
+    attributes: ['floorId'], // only select the 'floorId' field
+    group: 'floorId' // group by 'floorId' to remove duplicates
+  });
+  ctx.body = usersResult.map(user => user.floorId );
+});
+
+
+router.get("/api/communities/:communityId/buildings/:buildingId/units/:unitId/floors/:floorId/rooms", async (ctx) => {
+  if (!Users) {
+    ctx.throw(503, "Database is not ready, please try again later");
+  }
+  const usersResult = await Users.findAll({
+    where: { 
+      communityId: ctx.params.communityId,
+      buildingId: ctx.params.buildingId,
+      unitId: ctx.params.unitId,
+      floorId: ctx.params.floorId
+    },
+    attributes: ['roomId'], 
+    group: 'roomId'
+  });
+  ctx.body = usersResult.map(user => user.roomId );
+});
+
+// Get all users by room ID
+router.get("/api/communities/:communityId/buildings/:buildingId/units/:unitId/floors/:floorId/rooms/:roomId/users", async (ctx) => {
+  if (!Users) {
+    ctx.throw(503, "Database is not ready, please try again later");
+  }
+  const usersResult = await Users.findOne({
+    where: { 
+      communityId: ctx.params.communityId,
+      buildingId: ctx.params.buildingId,
+      unitId: ctx.params.unitId,
+      floorId: ctx.params.floorId,
+      roomId: ctx.params.roomId 
+    }
+  });
+    // 定义一个函数来隐藏部分姓名
+    function hidePartOfName(name) {
+      const length = name.length;
+      if (length <= 1) {
+        return "*";
+      } else if (length === 2) {
+        return name.substring(0, 1) + "*";
+      } else {
+        let newName = name.substring(0, 1);
+        for (let i = 1; i < length - 1; i++) {
+          newName += "*";
+        }
+        newName += name.substring(length - 1, length);
+        return newName;
+      }
+    }
+  
+    // 检查是否找到了用户，然后对姓名进行部分隐藏
+    if (usersResult && usersResult.contact) {
+      usersResult.contact = hidePartOfName(usersResult.contact);
+      usersResult.phone_number = hidePartOfName(usersResult.phone_number);
+    }
+  ctx.body = usersResult;
+});
+
+
+router.get("/api/users/:userId/details", async (ctx) => {
+  if (!Users) {
+    ctx.throw(503, "Database is not ready, please try again later");
+  }
+  const userResult = await Users.findOne({
+    where: { userId: ctx.params.userId }
+  });
+  
+  if (!userResult) {
+    ctx.throw(404, "User not found");
+  }
+  
+  ctx.body = userResult;
+});
+
+
+// // 更新计数
+// router.post("/api/count", async (ctx) => {
+//   const { request } = ctx;
+//   const { action } = request.body;
+//   if (action === "inc") {
+//     await Communities.create();
+//   } else if (action === "clear") {
+//     await Communities.destroy({
+//       truncate: true,
+//     });
+//   }
+
+//   ctx.body = {
+//     code: 0,
+//     data: await Communities.count(),
+//   };
+// });
+
+// // 获取计数
+// router.get("/api/count", async (ctx) => {
+//   const result = await Communities.count();
+
+//   ctx.body = {
+//     code: 0,
+//     data: result,
+//   };
+// });
 
 // 小程序调用，获取微信 Open ID
 router.get("/api/wx_openid", async (ctx) => {
